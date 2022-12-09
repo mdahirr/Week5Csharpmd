f1269838d583a594b5b518382068964c2d8eb6eb
mdahirr <m.dahir@hotmail.co.uk>
2022-12-09 13:56:31 +0000

    LINQ method exercies added from friday session

--- DBFirstSolution/DBFirstSolution/Program.cs
+++ DBFirstSolution/DBFirstSolution/Program.cs
@@ -1,4 +1,5 @@
-﻿using NorthwindData;
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindData;
 using NorthwindData.Models;
 
 using (var db = new NorthwindContext())
@@ -131,18 +132,145 @@ using (var db = new NorthwindContext())
     //    Console.WriteLine(customer.Name);
     //}
 
-    var query3 = db.Products
-        .OrderBy(c => c.QuantityPerUnit)
-        .ThenByDescending(c => c.ReorderLevel);
+    //var query3 = db.Products
+    //    .OrderBy(c => c.QuantityPerUnit)
+    //    .ThenByDescending(c => c.ReorderLevel);
 
-    foreach (var s in query3)
-    {
-        Console.WriteLine("QPU: {0}, RL: {1}", s.QuantityPerUnit, s.ReorderLevel);
-    }
+    //foreach (var s in query3)
+    //{
+    //    Console.WriteLine("QPU: {0}, RL: {1}", s.QuantityPerUnit, s.ReorderLevel);
+    //}
 
+    #endregion
+
+    #region loading and joining tables
+
+    //var orders = db.Orders
+    //    .Include(o => o.Customer)
+    //    .Include(o => o.OrderDetails)
+    //    .Where(o => o.Freight > 750);
+
+
+
+    //foreach (var order in orders)
+    //{
+    //    if (order.Customer != null)
+    //        Console.WriteLine($"Order {order.OrderId} " +
+    //            $"was made by {order.Customer.ContactName} " +
+    //            $"of {order.Customer.CompanyName}");
+
+    //    foreach (var item in order.OrderDetails)
+    //    {
+    //        Console.WriteLine($"    Product{item.ProductId} " + $"Quantity {item.Quantity}");
+    //    }
+
+    //Phil's solution
+
+    //foreach (var od in order.OrderDetails)
+    //{
+    //    Console.WriteLine($"\t Product: {od.ProductId} " + $"- Quantity {od.Quantity}");
+    //}
+    //}
+
+    //var orders = db.Orders
+    //    .Include(o => o.Customer)
+    //    .Include(o => o.OrderDetails)
+    //    .ThenInclude(od => od.Product)
+    //    .Where(o => o.Freight > 750)
+    //    .Select(o => o);
+
+
+
+    //foreach (var order in orders)
+    //{
+    //    Console.WriteLine($"Order {order.OrderId} " +
+    //            $"was made by {order.Customer.ContactName} " +
+    //            $"of {order.Customer.CompanyName}");
+
+
+
+    //    foreach (var od in order.OrderDetails)
+    //    {
+    //        Console.WriteLine($"\t Product: {od.ProductId} " +
+    //            $"- {od.Product.ProductName} " +
+    //            $"- Quantity {od.Quantity}");
+    //    }
+    //}
+    #endregion
+
+    #region Loading and joining tables
+    #region Q 1.1 (done)
+
+
+    //var customers = db.Customers
+    //    .Where(c => c.City == "London" || c.City == "Paris")
+    //    .Select(c => c);
+
+    //foreach (var item in customers)
+    //{
+    //    Console.WriteLine(
+    //        $"customerid: {item.CustomerId} " +
+    //        $"company name: {item.CompanyName} " +
+    //        $"address: {item.Address} " +
+    //        $"city: {item.City} " +
+    //        $"postalcode: {item.PostalCode} " +
+    //        $"country: {item.Country} "
+    //        );
+    //}
+    #endregion
+    #region Q 1.2 (done)
+
+    //var products = db.Products
+    //    .Where(p => p.QuantityPerUnit.Contains("bottles"))
+    //    .Select(p => p);
+
+    //foreach (var item in products)
+    //{
+    //    Console.WriteLine($"ProductName: {item.ProductName} " +
+    //        $"QuantityPerUnit: {item.QuantityPerUnit}");
+    //}
+    #endregion
+    #region Q 1.3 (done)
+    //var products = db.Products
+    //    .Where(p => p.QuantityPerUnit.Contains("bottles"))
+    //    .Include(p => p.Supplier);
+    ////.Select(p => p);
+
+    //foreach (var item in products)
+    //{
+    //    Console.WriteLine($"ProductName: {item.ProductName} " +
+    //        $"QuantityPerUnit: {item.QuantityPerUnit} " +
+    //        $"Company name: {item.Supplier.CompanyName} " +
+    //        $"Country: {item.Supplier.Country}");
+    //}
+    #endregion
+    #region Q 1.4
+
+    //var categories = db.Categories
+    //    .Include(c => c.Products)
+    //    .Where(c => c.) 
+    #endregion
+    #region Q 1.5 (done)
+    //var employees = db.Employees
+    //   .Where(e => e.Country.Contains("UK"));
+
+    //foreach (var item in employees)
+    //{
+    //    Console.WriteLine($"{item.TitleOfCourtesy} {item.FirstName} {item.LastName} lives in {item.City}");
+
+    //}
+    #endregion
+
+    #region Q 1.6
+
+    //var orders = db.Orders
+    //    .Where(o => o.Freight > 100 && o.ShipCountry.Contains("USA") || o.ShipCountry.Contains("UK"));
+
+    #endregion
     #endregion
 }
 
+#region method syntax cont
 //var nums = new List<int> { 3, 7, 1, 2, 8, 3, 0, 4, 5 };
 //Console.WriteLine(nums.Count(IsEven));
 //var numsQuery = nums.Count(n => n % 2 == 0);
@@ -177,3 +305,4 @@ using (var db = new NorthwindContext())
 //}
 
 //static bool IsEven(int x) => x % 2 == 0;
+#endregion
