23df22fe604a5d9dddf5e5bcf5e559ff6a2565e2
mdahirr <m.dahir@hotmail.co.uk>
2022-12-10 02:31:46 +0000

    ef model first continued

--- EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
+++ EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -10,7 +10,11 @@ namespace EF_ModelFirst
         {
             using (var db = new SouthwindContext())
             {
-
+                Customer cust = new Customer();
+                cust.CustomerId = "1";
+                cust.ContactName = "Mo";
+                cust.City = "London";
+                cust.PostalCode= "ab12cd";
             }
         }
     }
